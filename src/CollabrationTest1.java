6e1ee197878242aac853385403cba815837f2b0e
Gkenzhebekova96 <Brazil52@>
2023-05-29 15:27:58 +0400

    ER -1996 added last name

--- src/CollabrationTest1.java
+++ src/CollabrationTest1.java
@@ -1,6 +1,10 @@
 public class CollabrationTest1 {
     public static void main(String[] args) {
         String s ="Gul";
-        System.out.println("My name is "+ s);
+        String ss ="kenzhebekova";
+        System.out.println(ss+s);
+
+
+
     }
 }
