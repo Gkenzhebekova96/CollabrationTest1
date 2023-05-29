0e78e87f6fbd7999113b511220a202f90cd73ace
Gkenzhebekova96 <Brazil52@>
2023-05-29 15:38:43 +0400

    ER -1996 added 1 more string

--- src/CollabrationTest1.java
+++ src/CollabrationTest1.java
@@ -2,6 +2,7 @@ public class CollabrationTest1 {
     public static void main(String[] args) {
         String s ="Gul";
         String ss=" Hello world";
-        System.out.println(ss+"My name is "+ s);
+        String ss1="Lets go to travel";
+        System.out.println(ss+" My name is "+ s+" "+ss1);
     }
 }
